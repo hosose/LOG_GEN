cbf0dc5dc5b2f2ee71fe514e47286fce14f262f6
hosose <hosose22@gmail.com>
2026-08-21 13:51:43 +0900

    feat: add Terraform infrastructure configuration for Flink and Sliver services

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,13 +7,14 @@ variable "aws_region" {
 variable "project_name" {
   description = "리소스 이름에 사용할 프로젝트명"
   type        = string
-  default     = "de-ai-07-loggen"
+  default     = "de-ai-25-loggen"
 }
 
 variable "vpc_cidr" {
   description = "VPC CIDR, fargate 전용"
   type        = string
-  default     = "10.20.0.0/16"
+  # 대역 수정 => 0 => 20
+  default = "10.20.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
@@ -32,9 +33,9 @@ variable "public_subnet_cidrs" {
 
 # fargate task CPU
 variable "task_cpu" {
-  description = "cpu unit => 512 == 0.5 vcpu"
+  description = "cpu unit => 512 == 0.5 vCPU"
   type        = number
-  default     = "512"
+  default     = 512
 }
 
 # fargate task MEMORY
@@ -53,14 +54,14 @@ variable "log_retention_days" {
 
 # ECS TASK가 ECR 이미지 사용시 태그 -> latest
 variable "image_tag" {
-  description = "task가 정의될 때 참고하는 태그명, 가장 최신"
+  description = "task가 정의될때 참고하는 태그명, 가징 최신"
   type        = string
   default     = "latest"
 }
 
 # [브론즈 추가]
 # Kinesis Data Streams : KDS
-# 성능 영향 -> shard 개수, 데이터 보관 기간(retention period)
+# 성능 영향 -> shard 개수, 데이터 보관 기간(retention)
 # 프로비저닝 방식으로 구성한다 -> 샤드수 직접 지정 <-> 온디맨드 (자율구성)
 variable "kinesis_shard_count" {
   description = "KDS's shard count"
@@ -68,24 +69,90 @@ variable "kinesis_shard_count" {
   default     = 1
 }
 
+# 전송되지 않은 데이터는 하루만 보관하겠다!!
 variable "kinesis_retention_hour" {
-  description = "KDS data retention period in hours"
+  description = "KDS's retention period in hours"
   type        = number
   default     = 24
 }
 
+
 # Amazon Data Firehose : ADF
-# 최소 1 MiB, 최대 128 MiB입니다. 5 MiB을(를) 권장합니다.
-# 결과는 빠르게 볼 수 있다 -> 향후 조절 필요
+# 최소 1 MiB, 최대 128 MiB입니다. 5 MiB을(를) 권장
+# 결과는 빠르게 볼수 있다 -> 향후 조절 필요
 variable "firehose_buffer_size" {
   description = "해당 크기만큼 데이터가 쌓이면 강제 전송"
   type        = number
   default     = 1
 }
-
-# 최소 0 초, 최대 900 초입니다. 300 초을(를) 권장합니다.
+# 최소 0 초, 최대 900 초입니다. 300 초을(를) 권장
 variable "firehose_buffer_interval" {
   description = "해당 시간만큼 데이터가 쌓이면 강제 전송"
   type        = number
   default     = 60
+}
+
+
+# [실버 추가]
+# 실버 레이어에서 출력용 사드수
+variable "silver_kinesis_shard_count" {
+  description = "KDS's shard count"
+  type        = number
+  default     = 1
+}
+# kinesis 에서 미전송된 데이터 보관기간
+variable "silver_kinesis_retention_hour" {
+  description = "KDS's retention period in hours"
+  type        = number
+  default     = 24
+}
+
+# PyFlink 버전(런타임 환경의 버전) 1.20 사용
+variable "flink_runtime_environment" {
+  description = "Managed Service for Apache Flink의 런타임 환경버전"
+  type        = string
+  default     = "FLINK-1_20"
+}
+# Flink 어플리케이션의 병렬구성수
+# 현재는 1을 기본값, 최소 실행 단위
+variable "flink_parallelism" {
+  description = "Initial Flink application parallelism"
+  type        = number
+  default     = 1
+}
+# KPU(Kinesis Processing Unit) 하나당 Parallel task 수 설정
+# 기본 컴퓨팅의 과금단위
+variable "flink_parallelism_per_kpu" {
+  description = "Flink parallel tasks per KPU"
+  type        = number
+  default     = 1
+}
+# flink 은 실행 시켜두어야만 실제 처리가 됨
+# true : 인프라 적용되면 => 실행 => 실습 편의상 설정
+# false : 실제 사용시 적용
+variable "flink_start_application" {
+  description = "Whether Terraform should start the Managed Flink application"
+  type        = bool
+  default     = true
+}
+# Flink를 가동한후 입력쪽(브론즈향) kinesis에서 데이터 읽을때 어디서 부터 처리할것인가? 설정
+# 데이터는 계속해서 전송중 -> 추후 flink 가동 
+# -> 가동 전에 도달한 데이터도 처리할것인가? flink 가동 이후 도착한 데이터만 처리할것인가?
+# LATEST : flink 가둥 후 들어오는 데이터만 처리가
+# TRIM_HORIZON : kinesis에 남아 있는 과거 로그 데이터 모두 처리 -> 재처리/테스트/전체 데이터(이전) 처리
+
+variable "flink_source_init_poisition" {
+  description = "flink가 데이터 처리시 입력원쪽의 어디서부터 처리할 것인가 설정"
+  type        = string
+  default     = "LATEST"
+
+  # 변수의 값으로 올수 있는 내용들을 제약
+  validation {
+    # 오직 2가지만 허가됨
+    condition = contains([
+      "LATEST",
+      "TRIM_HORIZON"
+    ], var.flink_source_init_poisition)
+    error_message = "flink_source_init_poisition is only LATEST or TRIM_HORIZON"
+  }
 }
