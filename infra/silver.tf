408cb98d38b31f16a8504f398939a1be68174d87
hosose <hosose22@gmail.com>
2026-08-25 10:56:46 +0900

    feat: add silver layer infrastructure configuration and update associated documentation

--- infra/silver.tf
+++ infra/silver.tf
@@ -20,7 +20,7 @@ resource "aws_kinesis_firehose_delivery_stream" "silver" {
   name        = local.silver_firehose_name
   destination = "extended_s3"
 
-  # 입력소스 (키네시스, 역할 설정)
+  # 입력소스 (키네시스, 역활 설정)
   kinesis_source_configuration {
     kinesis_stream_arn = aws_kinesis_stream.silver.arn
     role_arn           = aws_iam_role.firehose_silver.arn
@@ -30,7 +30,7 @@ resource "aws_kinesis_firehose_delivery_stream" "silver" {
   extended_s3_configuration {
     # 버킷
     bucket_arn = aws_s3_bucket.data.arn
-    # 역할
+    # 역활
     role_arn = aws_iam_role.firehose_silver.arn
 
     # 버퍼 관련 용량, 시간 설정
@@ -38,12 +38,35 @@ resource "aws_kinesis_firehose_delivery_stream" "silver" {
     buffering_interval = var.firehose_buffer_interval # 60초
 
     # 데이터를 모아둔상태(버퍼링)에서 기록 -> 포멧
+    # [GLUE] Firehose가 JSON을 Parquet로 변환 처리함, S3에 자체 압축 옵션은 UNCOMPRESSED로 표기
+    compression_format = "UNCOMPRESSED"
     # 데이터 레코드 압축
-    # compression_format = "UNCOMPRESSED" # 1차는 원본 지정, 활성화되지 않음
-    compression_format = "GZIP" # GZIP으로 압축
+    #compression_format = "GZIP" # GZIP으로 압축
 
     # S3 버킷 및 S3 오류 출력 접두사 시간대
     custom_time_zone = "Asia/Seoul"
+
+    # [GLUE] 컨버전에 대한 구성 설정 (JSON => Glue Schema(사전에 정의된 테이블/스키마 <- 데이터구조/타입) => parquet)
+    data_fotmat_conversion_configuration {
+      # 구성 정보 사용
+      enabled = true
+
+      # 입력원 flink 통해서 나온 JSON임
+      input_foramt_configuration {
+
+      }
+
+      # JONS->parquet 변환시 참고할 스키마
+      schema_configuration {
+
+      }
+
+      # 출력 SNAPPY 압축을 통한 Parquet임
+      output_foramt_configuration {
+
+      }
+
+    }
 
     # 아래 처럼 구성 => partition pruning => Athena/opensearch/Glue/spark등 열기반으로 데이터 추출 유용
     # S3 버킷 접두사
